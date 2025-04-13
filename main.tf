2400eb327464d86ee9d84672e7e4f0f094f2d0bc
Ali Naveed <114981749+alinaveed1983@users.noreply.github.com>
2025-04-13 16:33:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ module "vpc" {
 module "ec2_bastion" {
   source        = "./modules/ec2"
   ami_id        = "ami-00a929b66ed6e0de6"
-  instance_name = "web-server"
+  instance_name = "rds-bastion"
   instance_type = "t3.micro"
   subnet_id     = module.vpc.public_subnet_1_id
 }
