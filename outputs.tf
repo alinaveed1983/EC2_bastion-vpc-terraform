1661fe01a913b256e57f0ff1f83bf2a6c73fac6f
Ali Naveed <114981749+alinaveed1983@users.noreply.github.com>
2025-04-13 17:57:06 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,12 @@ output "vpc_id" {
   value = module.vpc.vpc_id
 }
 
-output "bastion_ec2_id" {
-  value = module.ec2_bastion.instance_id
+output "bastion_ec2_1_id" {
+  value = module.ec2_bastion_1.instance_id
 }
+
+/*
+output "bastion_ec2_2_id" {
+  value = module.ec2_bastion_2.instance_id
+}
+*/
