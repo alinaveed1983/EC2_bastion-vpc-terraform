17d1196edea87c802d55794e9cb8479431d66065
Ali Naveed <114981749+alinaveed1983@users.noreply.github.com>
2025-04-13 16:32:46 +0530

    Update main.tf

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,1 +1,40 @@
+# IAM Role
+resource "aws_iam_role" "ec2_ssm_role" {
+  name = "${var.instance_name}-ssm-role"
 
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+# IAM Policy Attachment
+resource "aws_iam_role_policy_attachment" "ssm_core" {
+  role       = aws_iam_role.ec2_ssm_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+# IAM Instance Profile
+resource "aws_iam_instance_profile" "ec2_profile" {
+  name = "${var.instance_name}-profile"
+  role = aws_iam_role.ec2_ssm_role.name
+}
+
+# EC2 Instance
+resource "aws_instance" "this" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  subnet_id     = var.subnet_id
+
+  iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
+
+  tags = {
+    Name = var.instance_name
+  }
+}
