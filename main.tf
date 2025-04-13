060943188683c74eeabddd3de7a09a1c2da5f2ab
Ali Naveed <114981749+alinaveed1983@users.noreply.github.com>
2025-04-13 17:24:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,22 @@
 module "vpc" {
   source = "./modules/vpc"
+
+  vpc_cidr_block        = var.vpc_cidr_block
+  vpc_name              = var.vpc_name
+  public_subnet_1_cidr  = var.public_subnet_1_cidr
+  public_subnet_2_cidr  = var.public_subnet_2_cidr
+  private_subnet_1_cidr = var.private_subnet_1_cidr
+  private_subnet_2_cidr = var.private_subnet_2_cidr
+  nat_gw_name           = var.nat_gw_name
+  igw_name              = var.igw_name
+  main_rt_name          = var.main_rt_name
+  private_rt_name       = var.private_rt_name
 }
 
 module "ec2_bastion" {
   source        = "./modules/ec2"
-  ami_id        = "ami-00a929b66ed6e0de6"
-  instance_name = "rds-bastion"
-  instance_type = "t3.micro"
+  ami_id        = var.ami_id
+  instance_name = var.instance_name
+  instance_type = var.instance_type
   subnet_id     = module.vpc.public_subnet_1_id
 }
