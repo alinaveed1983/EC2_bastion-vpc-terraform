7e54a8224a7b0160d3180aa4b41241899cd75874
Ali Naveed <114981749+alinaveed1983@users.noreply.github.com>
2025-04-13 16:32:11 +0530

    Add files via upload

---
+++ modules/ec2/variable.tf
@@ -0,0 +1,16 @@
+variable "ami_id" {
+  description = "AMI ID for EC2 instance"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  default     = "t3.micro"
+}
+
+variable "subnet_id" {
+  description = "Subnet ID where EC2 will be created"
+}
+
+variable "instance_name" {
+  description = "Name tag for EC2 instance"
+}
