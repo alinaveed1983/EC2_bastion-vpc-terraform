7f2db9f846b337b63885d9d8477e46f0f15c1ebf
Ali Naveed <114981749+alinaveed1983@users.noreply.github.com>
2025-04-16 10:13:16 +0530

    Add files via upload

---
+++ modules/ec2/variable.tf
@@ -0,0 +1,23 @@
+variable "ami_id" {
+  description = "AMI ID for EC2 instance"
+  default     = "ami-00a929b66ed6e0de6"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  default     = "t3.micro"
+}
+
+variable "subnet_id" {
+  description = "Subnet ID where EC2 will be created"
+}
+
+variable "instance_name" {
+  description = "Name tag for EC2 instance"
+  default     = "rds-ec2"
+}
+
+variable "instance_profile_name" {
+  description = "IAM instance profile name to be attached for SSM access"
+  default     = null
+}
